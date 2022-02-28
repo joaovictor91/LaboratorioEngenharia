872fcef87cb5158b7b154b6d86da61e8aef2d24b
joaovictor91 <j-victor-franco@live.com>
2022-02-28 13:58:36 -0300

    Realizado no meu PC

---
+++ AplicacaoWeb/AplicacaoWeb/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AplicacaoWeb.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias{ get; set; }
+
+    }
+}
